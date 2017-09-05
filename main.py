572b44cb24d0337620c9d86833d40c2adb88a4d4
jajajanrey <evangelistajohnrey@gmail.com>
2017-09-05 15:43:08 +0800

    add discovery url

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 import json
 
 from oauth2client.service_account import ServiceAccountCredentials
-from apiclient.discovery import build
+from googleapiclient import discovery
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
 CREDENTIALS_PATH = '/tmp/credentials.json'
+SHEETS_DISCOVERY_URL = 'https://sheets.googleapis.com/$discovery/rest?version=v4'
 
 
 def get_credentials(credentials):
@@ -18,7 +19,8 @@ def get_credentials(credentials):
     with open(CREDENTIALS_PATH, "w") as credentials_file:
         credentials_file.write(credentials)
 
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_PATH, SCOPES)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        CREDENTIALS_PATH, SCOPES)
 
     return credentials
 
@@ -33,14 +35,17 @@ def main(sheet_id, range_notation_list, service_account_json):
     else:
         range_notation_list = ["A:B"]
 
-    service = build('sheets', 'v4', credentials=credentials)
+    service = discovery.build('sheets', 'v4',
+                              credentials=credentials,
+                              discoveryServiceUrl=SHEETS_DISCOVERY_URL)
     result = None
 
     try:
         result = service.spreadsheets().values().batchGet(
-        spreadsheetId=sheet_id, ranges=range_notation_list).execute()
+            spreadsheetId=sheet_id, ranges=range_notation_list).execute()
     except Exception:
         result = None
 
     os.remove(CREDENTIALS_PATH)
+
     return result
